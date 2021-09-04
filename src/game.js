6fcaf3cfea84360adcf5a3aaea0e7d28f197c43b
dagp-ucol <dgentner@ucol.mx>
2021-09-04 16:56:52 -0500

    🟠 unstable commit

--- src/game.js
+++ src/game.js
@@ -10,13 +10,44 @@ export default class Game {
 
     _generarTablero() {
         let tablero = new Array;
-        for (let i = 0; i <= 100; i++) {
-            for (let z = 0; z < 4; z++) {
+        for (let i = 1; i <= 20; i++) {
+            for (let z = 1; z < 5; z++) {
                 tablero.push(false);
             }
             tablero.push(true);
         }
+        tablero.pop();
+        tablero.push(null);
         return tablero;
+    }
+
+    runGame() {
+        while (this._players.getPlayerAPos() < 100 && this._players.getPlayerBPos() < 100) {
+            
+            this._players.movePlayerA(this._dado.lanzar());
+            this._checkCasilla(this.players.getPlayerAPos, 0);
+            this._players.movePlayerB(this._dado.lanzar());
+            this._checkCasilla(this.players.getPlayerBPos, 1);
+
+        }
+    }
+
+    _checkCasilla(value) {
+        
+        if (this._tablero[value - 1] === false) {
+            return;
+        } else {
+            if (this._tablero[value - 1] === true) {
+                this.serpienteOrEscalera()
+            } else {
+                
+            }
+        }
+
+    }
+
+    serpienteOrEscalera() {
+
     }
 
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Game from "./game.js";
-import Players from "./players.js";
+import Player from "./player.js";
 
-let jugadores  = new Players('Carlos', 'Mariana');
+let jugadores  = new Player('Carlos', 'Mariana');
 let partida = new Game(jugadores);
 
 console.log(partida._tablero);
